77de212e55b95261d6c55ee498b203fd7ba2472a
Burchard36 <burchard36@gmail.com>
2023-06-22 21:33:17 -0700

    finish update checker

--- MusePluseBase/src/main/java/com/burchard36/musepluse/utils/UpdateChecker.java
+++ MusePluseBase/src/main/java/com/burchard36/musepluse/utils/UpdateChecker.java
@@ -1,5 +1,6 @@
 package com.burchard36.musepluse.utils;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 
@@ -16,18 +17,17 @@ import static com.burchard36.musepluse.utils.StringUtils.convert;
 
 public class UpdateChecker {
 
-    private final static String VERSION = "2.0.1";
+    private final static String VERSION = "2.0.2";
 
     public static void checkVersion(final Player player, final Consumer<Void> callback) {
         CompletableFuture.runAsync(() -> {
             try {
                 HttpClient client = HttpClient.newHttpClient();
                 HttpRequest request = HttpRequest.newBuilder()
-                        .uri(new URI("https://github.com/Burchard36/MusePluse/raw/main/VERSION.txt"))
+                        .uri(new URI("https://raw.githubusercontent.com/Burchard36/MusePluse/main/VERSION.txt"))
                         .build();
                 HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
                 String versionString = response.body();
-
                 if (!versionString.equals(VERSION)) {
                     TaskRunner.runSyncTaskLater(() -> {
                         player.sendMessage(convert("&a&lHey!&a this is the author of &a&lMusePluse!"));
@@ -35,7 +35,7 @@ public class UpdateChecker {
                         TaskRunner.runSyncTaskLater(() -> {
                             player.playSound(player, Sound.ENTITY_VILLAGER_YES, 1.0f, 1.0f);
                             player.sendMessage(convert("&a&lA new update is out (%s)! &ehttps://www.spigotmc.org/resources/musepluse-youtube-music-player-massive-update.110536/".formatted(versionString)));
-                        }, 15);
+                        }, 35);
                     }, 60);
                 }
 
